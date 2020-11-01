2f29a2615ff9f317e6403032622b041a24ebb79b
Hamza Farooqui <hamzattking@gmail.com>
2020-11-01 23:23:47 +0500

    Updated menu-item with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+};
+
+// const CapsPage = (props) => {
+//   console.log(props);
+//   return(
+//   <div>
+//     <h1>CAPS PAGE</h1>
+//   </div>
+//   )
+// };
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* {}<HomePage /> */}
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
     </div>
   );
 }
