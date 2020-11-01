5688c7bc09bfa4cbaad5a094bbcb8652819fd5a5
Hamza Farooqui <hamzattking@gmail.com>
2020-11-02 03:40:12 +0500

    Added a shop page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = (props) => {
-  console.log(props);
-  return(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
-};
-
-// const CapsPage = (props) => {
-//   console.log(props);
-//   return(
-//   <div>
-//     <h1>CAPS PAGE</h1>
-//   </div>
-//   )
-// };
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      {/* {}<HomePage /> */}
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
