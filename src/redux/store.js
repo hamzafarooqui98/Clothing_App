2483a20b49956736d54f88e3316c0f42d5c83721
Hamza Farooqui <hamzattking@gmail.com>
2020-11-26 19:10:25 +0500

    Added redux-persistor to have localstorage type functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';  //This is used when debugging our redux code
 
+import { persistStore } from 'redux-persist';  //It allows our browser to cache our store depending on certain configuration
+
 import rootReducer from './root-reducer';
 
 //The middleware store is expecting from redux is going to be an array
 const middlewares = [logger];  //we may want to modify it later thats why we have putted in a array which is used below
 
-const store = createStore( rootReducer, applyMiddleware( ...middlewares ));  //spread operator will spread all the value that are in the logger which is in the array of middlewares
+export const store = createStore( rootReducer, applyMiddleware( ...middlewares ));  //spread operator will spread all the value that are in the logger which is in the array of middlewares
 
-export default store;
+export const persistor = persistStore( store );
+
+export default { store, persistor };
